460d5ddb766b58ff146d2f9f1d1b7f2d189fb991
ilcho.vuchkov <ilcho.vuchkov@bulpros.com>
2019-09-12 11:27:15 +0300

    Issue #3080780 by ilchovuchkov: Update UsersForm arrays.

--- src/Form/UsersForm.php
+++ src/Form/UsersForm.php
@@ -67,64 +67,64 @@ class UsersForm extends ConfigFormBase {
     $config = $this->config('notify.settings');
 
     $form['#tree'] = TRUE;
-    $form['info'] = array(
+    $form['info'] = [
       '#markup' => '<p>' . $this->t('The following table shows all users that have notifications enabled:') . '</p>',
-    );
+    ];
 
-    $form['users'] = array();
+    $form['users'] = [];
 
     // Fetch users with notify enabled.
     $q = \Drupal::database()->select('notify', 'n');
     $q->join('users', 'u', 'n.uid = u.uid');
     $q->join('users_field_data', 'v', 'n.uid = v.uid');
-    $q->fields('v', array('uid', 'name', 'mail', 'langcode'));
-    $q->fields('n', array('status', 'node', 'comment', 'attempts', 'teasers'));
+    $q->fields('v', ['uid', 'name', 'mail', 'langcode']);
+    $q->fields('n', ['status', 'node', 'comment', 'attempts', 'teasers']);
     $q->condition('n.status', 1);
     $q->condition('v.status', 1);
     $q->orderBy('v.name');
     $uresult = $q->execute();
 
     foreach ($uresult as $user) {
-      $form['users'][$user->uid] = array();
-      $form['users'][$user->uid]['name'] = array(
+      $form['users'][$user->uid] = [];
+      $form['users'][$user->uid]['name'] = [
         '#markup' => $user->name,
-      );
-      $form['users'][$user->uid]['mail'] = array(
+      ];
+      $form['users'][$user->uid]['mail'] = [
         '#markup' => $user->mail,
-      );
-      $form['users'][$user->uid]['node'] = array(
+      ];
+      $form['users'][$user->uid]['node'] = [
         '#type' => 'checkbox',
         '#default_value' => $user->node,
-      );
-      $form['users'][$user->uid]['comment'] = array(
+      ];
+      $form['users'][$user->uid]['comment'] = [
         '#type' => 'checkbox',
         '#default_value' => $user->comment,
-      );
-      $form['users'][$user->uid]['teasers'] = array(
+      ];
+      $form['users'][$user->uid]['teasers'] = [
         '#type' => 'select',
         '#default_value' => $user->teasers,
-        '#options' => array(
+        '#options' => [
           $this->t('Title only'),
           $this->t('Title + Teaser'),
           $this->t('Title + Body'),
           $this->t('Title + Body + Fields'),
-        ),
-      );
-      $form['users'][$user->uid]['attempts'] = array(
+        ],
+      ];
+      $form['users'][$user->uid]['attempts'] = [
         '#markup' => $user->attempts ? intval($user->attempts) : 0,
-      );
+      ];
     }
 
-    $form['info2'] = array(
+    $form['info2'] = [
       '#markup' => '<p>' . $this->t('You may check/uncheck the checkboxes and the &#8220;How much&#8220;-selection to change the users\' subscription. Press &#8220;Save settings&#8220; to save the settings.') . '</p>',
-    );
+    ];
 
-    $form['bulk'] = array(
+    $form['bulk'] = [
       '#title' => $this->t('Bulk subscribe all users'),
       '#type' => 'checkbox',
       '#default_value' => FALSE,
       '#description' => $this->t('Subscribe all non-blocked users that do not already subscribe to notifications.'),
-    );
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -141,12 +141,12 @@ class UsersForm extends ConfigFormBase {
       $teasers = $config->get('notify_def_teasers');
 
       $r = \Drupal::database()->select('notify', 'n');
-      $r->fields('n', array('uid'));
+      $r->fields('n', ['uid']);
       $r->execute();
 
       $q = \Drupal::database()->select('users', 'u');
       $q->join('users_field_data', 'v', 'u.uid = v.uid');
-      $q->fields('v', array('uid','name'));
+      $q->fields('v', ['uid','name']);
       $q->condition('u.uid', 0, '>');
       $q->condition('v.status', 1, '=');
       $q->condition('u.uid', $r, 'NOT IN');
@@ -154,14 +154,14 @@ class UsersForm extends ConfigFormBase {
 
       foreach ($result as $record) {
         \Drupal::database()->insert('notify')
-          ->fields(array(
+          ->fields([
             'uid' => $record->uid,
             'status' => 1,
             'node' => $node,
             'comment' => $comment,
             'teasers' => $teasers,
             'attempts' => 0,
-          ))
+          ])
           ->execute();
       }
     }
@@ -172,12 +172,12 @@ class UsersForm extends ConfigFormBase {
     if (isset($values['users']) && $values['users']) {
       foreach ($values['users'] as $uid => $settings) {
         \Drupal::database()->update('notify')
-          ->fields(array(
+          ->fields([
             'node' => $settings['node'],
             'teasers' => $settings['teasers'],
             'comment' => $settings['comment'],
             // 'attempts' => $settings['attempts'],
-          ))
+          ])
           ->condition('uid', $uid)
           ->execute();
       }
